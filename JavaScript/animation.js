2f8a33ce5aa1f81eefb2ae74c6f77ee22a0b339f
Raxabi <beltrandiaztorres@gmail.com>
2022-02-18 04:11:42 +0100

    Adding: Animation for some html elements on index page, _blank target to any href and url to RArchxbi official site

---
+++ JavaScript/animation.js
@@ -0,0 +1,11 @@
+gsap.to("#comtitle", {
+    duration: 35,
+    y: 700,
+    opacity: 0,
+    ease: "bounce"
+});
+
+gsap.to("#fcontent", {
+    duration: 5,
+    y: -40,
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# This is the official Site of Sibani Proyect GitHub Repository
+The Sibani Proyect is an Open source community.
